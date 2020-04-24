e3be0eddf5b9fbcac9fbf22e7cc42acab7f3f5e2
aascis <s.aascis9@gmail.com>
2020-04-24 13:30:31 +0545

    testing gibhub for first time

---
+++ script.js
@@ -0,0 +1,22 @@
+var object = {
+    username : "Ashish",
+    Password : "12345678"
+}
+
+var database  = [object];
+
+var newsfeed = [
+    {
+        username : "urusha",
+        timeline : "time"
+    },
+    {
+        username : "sally",
+        timeline : "javascript is cool"
+    },
+    {
+        username : "mitch",
+        timeline : "cool!!"
+    }
+
+]
